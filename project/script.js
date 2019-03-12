d53b14696e0c62b3efa59ff678588811bf54dbbe
Alex <apk.work.13@gmail.com>
2019-03-12 16:18:04 +0200

    Это наш первый коммит

---
+++ project/script.js
@@ -0,0 +1,19 @@
+/*let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+} */// простой объект
+
+//console.log(persone.name);
+//console.log(persone ["age"]);
+
+//let arr = ["plum.png","orange.jpg","apple.bmp"];
+
+//console.log(arr[1]);
+/*let a = confirm ("Hello");
+console.log(a);*/
+let a = true,
+    b = true;
+console.log(a&&b);
+
+
