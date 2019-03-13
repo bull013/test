5c0ed728a6107c924c316f53a5178e1c1cdac807
Alex <apk.work.13@gmail.com>
2019-03-13 14:13:14 +0200

    Первый этап практики

--- project/script.js
+++ project/script.js
@@ -9,11 +9,22 @@
 
 //let arr = ["plum.png","orange.jpg","apple.bmp"];
 
-console.log(arr[1]);
-/*let a = confirm ("Hello");
+/*console.log(arr[1]);
+let a = confirm ("Hello");
 console.log(a);*/
-let a = true,
+/*let a = true,
     b = true;
 console.log(a&&b);
+let n=40;
+while (n<50){
+    console.log(n);
+    n++;
+}*/
+/*let n=40;
+do{
+    console.log(n);
+    n++;
+}while (n<50);*/
 
+alert( 0 || "" || 2 || undefined || true || falsе);
 
---
+++ project/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let n=40;
+do{
+    console.log(n);
+    n++;
+}while (n<50);
